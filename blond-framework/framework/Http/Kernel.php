4ee06a44c76c093f09f1a335b3a73ee0b47f4adb
sunlazor <1979844+sunlazor@users.noreply.github.com>
2025-07-08 23:04:34 +0300

    BF: Lesson Routes adding

--- blond-framework/framework/Http/Kernel.php
+++ blond-framework/framework/Http/Kernel.php
@@ -2,13 +2,36 @@
 
 namespace Sunlazor\BlondFramework\Http;
 
+use FastRoute\RouteCollector;
+
+use function FastRoute\simpleDispatcher;
+
 class Kernel
 {
 
     public function handle(Request $request): Response
     {
-        $content = '<h1>Content!</h1>';
+        $dispatcher = simpleDispatcher(function (RouteCollector $rc) {
+            $rc->addRoute('GET', '/', function () {
+                $content = '<h1>Content!sss</h1>';
 
-        return new Response($content);
+                return new Response($content);
+            });
+
+            $rc->get('/test/{id}', function (array $vars) {
+                $content = "<h1>Tesst = {$vars['id']}</h1>";
+
+                return new Response($content);
+            });
+        });
+
+        $routeInfo = $dispatcher->dispatch(
+            $request->server['REQUEST_METHOD'],
+            $request->server['REQUEST_URI'],
+        );
+
+        [$status, $handler, $vars] = $routeInfo;
+
+        return $handler($vars);
     }
 }
