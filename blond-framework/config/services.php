4e6680803a4c7b15b14fda5c3a231ffcc9cce8dc
sunlazor <1979844+sunlazor@users.noreply.github.com>
2025-08-14 16:23:37 +0200

    BF: Adding League Container using

--- blond-framework/config/services.php
+++ blond-framework/config/services.php
@@ -1,5 +1,14 @@
 <?php
 
-$container = new League\Container\Container;
+use League\Container\Container;
+use Sunlazor\BlondFramework\Http\Kernel;
+use Sunlazor\BlondFramework\Routing\Router;
+use Sunlazor\BlondFramework\Routing\RouterInterface;
+
+$container = new Container();
+
+$container->add(RouterInterface::class, Router::class);
+
+$container->add(Kernel::class)->addArgument(RouterInterface::class);
 
 return $container;
--- blond-framework/public/index.php
+++ blond-framework/public/index.php
@@ -6,14 +6,14 @@ define('BASE_PATH', dirname(__DIR__));
 
 include_once BASE_PATH . '/vendor/autoload.php';
 
+use League\Container\Container;
 use Sunlazor\BlondFramework\Http\Kernel;
 use Sunlazor\BlondFramework\Http\Request;
-use Sunlazor\BlondFramework\Routing\Router;
 
+/** @var Container $container */
 $container = require_once BASE_PATH . '/config/services.php';
 
-$router = new Router();
-$kernel = new Kernel($router);
+$kernel = $container->get(Kernel::class);
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
